529f5cac98b570dc1d9a013bffe955cdb72126ca
narayangt <narayan.mail12@gmail.com>
2014-09-18 14:10:05 +1000

    Create configMain.php

---
+++ configMain.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!isset($_SESSION))
+{
+	 session_start();
+}
+
+
+//Object to connect database narayan_userdb
+class maindb_con
+{
+    private $hostname;
+    private $username;
+    private $password;
+    private $dbname;
+
+    function __construct()
+    {
+        $this->hostname = "**********";     // hostname
+        $this->username = "**********";     // username
+        $this->password = "**********";     // password
+        $this->dbname = "**********";       // database name that would be initially selected after sucessful connection
+        if(!mysql_connect($this->hostname, $this->username, $this->password,$this->dbname))
+        {
+        	echo'Error:: 1001, please change credintial properties';               // special error code to represent connection error
+        	exit();
+        }
+    }
+    /*function maindb_con($hname, $uname, $pwd, $dbname)
+    {
+        $this->hostname = $hname;
+        $this->username = $uname;
+        $this->password = $pwd;
+        $this->dbname = $dbname;
+    }*/
+    function get_con()
+    {
+        $con=mysql_connect($this->hostname, $this->username, $this->password);
+        mysql_select_db($this->dbname,$con);
+        return $con;
+    }
+    function get_dbname()
+    {
+    	return $this->dbname;
+    }
+} 
+?>
